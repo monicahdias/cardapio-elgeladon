5a65d0a0ce161ab07e389b8cae14e17a111a3807
Monica Sumie Hieda Dias <msumiedias@gmail.com>
2022-04-27 19:56:22 -0300

    paletas

--- src/services/PaletaService.js
+++ src/services/PaletaService.js
@@ -2,9 +2,25 @@ import { Api } from "helpers/Api";
 
 const parseResponse = (response) => response.json();
 
+const transformPaleta = (paleta) => {
+  const [sabor, recheio] = paleta.sabor.split(" com ");
+
+  return {
+    ...paleta,
+    id: paleta._id,
+    titulo: paleta.sabor,
+    sabor,
+    ...(recheio && { recheio }),
+    possuiRecheio: Boolean(recheio),
+  };
+};
+
+const parseTransformLista = (response) =>
+  parseResponse(response).then((paletas) => paletas.map(transformPaleta));
+
 export const PaletaService = {
   getLista: () =>
-    fetch(Api.paletaLista(), { method: "GET" }).then(parseResponse),
+    fetch(Api.paletaLista(), { method: "GET" }).then(parseTransformLista),
   getById: (id) =>
     fetch(Api.paletaById(id), { method: "GET" }).then(parseResponse),
   create: () =>
